97c5dffe68e7d34880f4cb4d763ed39d6b4525c1
Mike Wickett <mike@sagecomm.com>
2017-03-10 21:52:55 -0500

    Hooked up contentful

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
+require('dotenv').config({ silent: true})
+
 const htmlStandards = require('reshape-standard')
 const cssStandards = require('spike-css-standards')
 const jsStandards = require('babel-preset-env')
 const pageId = require('spike-page-id')
+const Contentful = require('spike-contentful')
+const marked = require('marked')
+const locals = {}
 
 module.exports = {
   devtool: 'source-map',
@@ -11,8 +16,25 @@ module.exports = {
   },
   ignore: ['**/layout.sgr', '**/_*', '**/.*', '_cache/**', 'readme.md', 'yarn.lock'],
   reshape: htmlStandards({
-    locals: (ctx) => { return { pageId: pageId(ctx), foo: 'bar' } }
+    locals: (ctx) => { return Object.assign(locals, { pageId: pageId(ctx) }, { marked: marked }) }
   }),
   postcss: cssStandards(),
-  babel: { presets: [[jsStandards, { modules: false }]] }
+  babel: { presets: [[jsStandards, { modules: false }]] },
+  plugins: [
+      new Contentful({
+          addDataTo: locals,
+          accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+          spaceId: process.env.CONTENTFUL_SPACE_ID,
+          contentTypes: [
+              {
+                  name: 'home',
+                  id: 'home',
+              },
+              {
+                  name: 'contactPage',
+                  id: 'contactPage',
+              },
+          ]
+      })
+  ]
 }
