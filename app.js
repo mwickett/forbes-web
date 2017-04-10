3d1b197f02d9b7d727636802538e08d89116fedc
Mike Wickett <mike@sagecomm.com>
2017-04-10 17:23:31 -0400

    Add read more link for events

--- app.js
+++ app.js
@@ -26,30 +26,6 @@ const slugify = function(text) {
     .replace(/-+$/, "")
 }
 
-// const getAddress = function(lat, lon) {
-//   // return new pending promise
-//   return new Promise((resolve, reject) => {
-//     // select http or https module, depending on reqested url
-//     const request = https.get(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lon}&key=${googleMapsApiKey}`, (response) => {
-//       // handle http errors
-//       if (response.statusCode < 200 || response.statusCode > 299) {
-//          reject(new Error('Failed to load page, status code: ' + response.statusCode));
-//        }
-//       // temporary data holder
-//       const body = [];
-//       // on every content chunk, push it to the data array
-//       response.on('data', (chunk) => body.push(chunk));
-//       // we are done, resolve promise with those joined chunks
-//       response.on('end', (body) => {
-//         console.log(body)
-//         resolve(body)
-//       });
-//     });
-//     // handle connection errors of the request
-//     request.on('error', (err) => reject(err))
-//     })
-// };
-
 function reverseLookup(lat, lon) {
   return axios.get(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lon}&key=${googleMapsApiKey}`)
     .then(function (response) {
@@ -129,7 +105,7 @@ module.exports = {
               },
               {
                 name: 'events',
-                id: 'event'
+                id: 'event',
               },
               {
                 name: 'basicPage',
