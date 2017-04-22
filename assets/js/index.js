19a306eed4389669c3c1c5ecf792da47e428dee8
Mike Wickett <mike@sagecomm.com>
2017-04-21 22:23:01 -0400

    Messing with modules

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,16 +1,19 @@
+//import { mobileNav } from './modules/nav.js'
 require('offline-plugin/runtime').install()
 
-function toggleNav (e) {
-  if (navActive === true) {
-    nav.style.display = 'none'
-    navActive = false
-  } else {
-    nav.style.display = 'block'
-    navActive = true
+document.addEventListener('DOMContentLoaded', function () {
+  function toggleNav (e) {
+    if (navActive === true) {
+      nav.style.display = 'none'
+      navActive = false
+    } else {
+      nav.style.display = 'block'
+      navActive = true
+    }
   }
-}
 
-let navActive = false
-const nav = document.getElementById('nav')
-const navButton = document.getElementById('navLink')
-navButton.addEventListener('click', toggleNav)
+  let navActive = false
+  const nav = document.getElementById('nav')
+  const navButton = document.getElementById('navLink')
+  navButton.addEventListener('click', toggleNav)
+})
