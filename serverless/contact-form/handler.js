51f60c8de579bb3898a364e491185581b8762724
Mike Wickett <mike@sagecomm.com>
2017-06-28 08:48:25 -0400

    Update contact form script

--- serverless/contact-form/handler.js
+++ serverless/contact-form/handler.js
@@ -3,34 +3,34 @@
 const AWS = require('aws-sdk')
 const ses = new AWS.SES()
 
-const RECEIVER = [ 'mike@sagecomm.com', 'mike@wickett.ca', 'mike@drforbes.ca' ]
+const RECEIVER = ['mike@wickett.ca', 'mike@drforbes.ca', 'lindsey@drforbes.ca']
 const SENDER = 'mike@drforbes.ca'
 
-module.exports.formEmail = function (event, context) {
-  console.log('Received event:', event)
-  sendEmail(event, function (err, data) {
-    context.done(err, null)
-  })
+module.exports.formEmail = function(event, context) {
+    console.log('Received event:', event)
+    sendEmail(event, function(err, data) {
+        context.done(err, null)
+    })
 }
 
-function sendEmail (event, done) {
-  const params = {
-    Destination: {
-      ToAddresses: RECEIVER
-    },
-    Message: {
-      Body: {
-        Text: {
-          Data: 'Name: ' + event.name + '\nEmail: ' + event.email + '\nDesc: ' + event.description,
-          Charset: 'UTF-8'
-        }
-      },
-      Subject: {
-        Data: 'Dr. Forbes & Associates Web Contact Form: ' + event.name,
-        Charset: 'UTF-8'
-      }
-    },
-    Source: SENDER
-  }
-  ses.sendEmail(params, done)
+function sendEmail(event, done) {
+    const params = {
+        Destination: {
+            ToAddresses: RECEIVER
+        },
+        Message: {
+            Body: {
+                Text: {
+                    Data: 'Name: ' + event.name + '\nEmail: ' + event.email + '\nDesc: ' + event.description,
+                    Charset: 'UTF-8'
+                }
+            },
+            Subject: {
+                Data: 'Dr. Forbes & Associates Web Contact Form: ' + event.name,
+                Charset: 'UTF-8'
+            }
+        },
+        Source: SENDER
+    }
+    ses.sendEmail(params, done)
 }
